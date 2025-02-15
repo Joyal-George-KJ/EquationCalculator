93cc01cf5d940bf8ccad21cd9914056faee2cf6c
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-02-15 13:06:02 +0530

    Price Changer Done. Moving to test phase

--- src/components/PriceChanger.jsx
+++ src/components/PriceChanger.jsx
@@ -7,17 +7,21 @@ function PriceChanger() {
     const priceData = useSelector((state) => state.priceList);
     const dispatch = useDispatch();
 
+    const handleChange = (value, key) => {
+        dispatch(setPriceList({ key, value }));
+    };
+
     return (
         <div
             className={`absolute ${
                 toggle ? "w-full h-full" : ""
             } bottom-0 right-0 p-4 bg-neutral-800 rounded-tl-lg`}
         >
-            <button type="button" className="absolute bottom-16 right-16 px-4 py-2 text-neutral-900 rounded-full bg-green-500" onClick={() => setToggle(!toggle)}>
+            <button type="button" className="absolute bottom-8 right-8 px-2 py-1 text-neutral-50 rounded-full bg-neutral-700 shadow-lg" onClick={() => setToggle(!toggle)}>
                 {toggle ? (
-                    <i className="bi bi-x-lg"></i>
+                    <i className="bi bi-x-lg font-mono font-bold"></i>
                 ) : (
-                    <i className="bi bi-currency-rupee"></i>
+                    <i className="bi bi-currency-rupee font-mono font-bold"></i>
                 )}
             </button>
             {toggle && (
@@ -35,8 +39,8 @@ function PriceChanger() {
                                 type="number"
                                 step={"any"}
                                 value={priceData[key]}
-                                onChange={() => {
-                                    useDispatch(setPriceList({ key, value: Number(document.getElementById(`display-price-${i}`).value) }));
+                                onChange={(e) => {
+                                    handleChange(e.target.value, key)
                                 }}
                                 placeholder="Price"
                                 className="p-2 text-neutral-50 bg-neutral-700 rounded-r w-3/4"
