08ecac4c3f23b2b4e95ad1e5f5a6b6e6a8496b9d
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-02-24 12:52:59 +0530

    Fixed: Accidentally removed reducer from the Store Caused the work flow of project

--- src/utilities/store.js
+++ src/utilities/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import readingSliceReducer from "./slice/readingSlice";
 import priceListReducer from "./slice/priceListSlice";
 
 const store = configureStore({
     reducer: {
+        calculateReading: readingSliceReducer,
         priceList: priceListReducer,
     }
 })
